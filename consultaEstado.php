35113905009f8b667e80c25f61d5195deadaaf8c
Jonathan Espinoza <jonathanespinozab@gmail.com>
2016-07-04 14:49:14 -0300

    J.E.B. Creación de páginas.

---
+++ consultaEstado.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <!--
+        Se debe ingresar rut para verificar la informacion a continuacion.
+        -
+        -
+        Se deben mostrar los sgtes mensajes segun el caso:
+        **Caso (estado pendiente)= "Estado de solicitud: Pendiente"
+        **Caso (estado aprobado)= "Estado de solicitud: Aprobado"
+        **Caso (estado rechazado)= "Estado de solicitud: Rechazado"
+        
+        "Para mas informacion puede llamarnos al numero que aparece en nuestra página oficial"
+        
+        -->
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ postulacion.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <!--
+        rut
+        nombre
+        ap paterno
+        ap materno
+        fecha nac.
+        sexo
+        telefono
+        email
+        direccion
+        comuna (despliega comunas stgo)
+        educacion (despliega profesional, tecnico, media, basica, no posee)
+        experiencia en programacion (si posee, debe ingresar cant. de años)
+        modalidad (despliega diurna y vespertina)
+        curso (despliega java, .net y php)
+        
+        -->
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
