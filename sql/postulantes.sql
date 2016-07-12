27d737d93580aa234ff67805dd669462b8efd553
jomipeca <jose.perez@peluka.info>
2016-07-12 13:14:02 -0400

    SQL 1.5.8 (varchar-->int)

--- sql/postulantes.sql
+++ sql/postulantes.sql
@@ -175,7 +175,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `apellido_mat` varchar(255) NOT NULL,
   `fecha_nacimiento` date NOT NULL,
   `sexo` varchar(20) NOT NULL,
-  `telefono` varchar(15) NOT NULL,
+  `telefono` int(15) NOT NULL,
   `email` varchar(255) NOT NULL,
   `direccion` varchar(255) NOT NULL,
   `id_comuna` int(5) NOT NULL,
@@ -197,9 +197,9 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 --
 
 INSERT INTO `usuarios` (`user_id`, `rut`, `nombre`, `password`, `apellido_pat`, `apellido_mat`, `fecha_nacimiento`, `sexo`, `telefono`, `email`, `direccion`, `id_comuna`, `educacion`, `experiencia`, `experiencia_anios`, `modalidad`, `curso`, `user_created_at`) VALUES
-(5, '123456789', 'Admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'admin', 'Administrador', '2016-06-09', 'X', '123456789', 'admin@admin.admin', 'calle falsa 123', 15, 1, true, 15, 1, 1, '2016-06-09 00:24:49'),
-(6, '177008796', 'Joseph', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Perez', 'Carmona', '1991-03-26', 'M', '50010078', 'jose.perez@peluka.info', 'antonio varas 666', 20, 2, true, 2, 2, 2, '2016-06-30 16:59:35'),
-(7, '186948483', 'Jonathan', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Espinoza', 'Brain', '1994-01-01', 'M', '23004500', 'jonathanespinozab@gmail.com', 'antonio varas 666', 35, 3, false, 0, 2, 3, '2016-06-30 16:59:35');
+(5, '123456789', 'Admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'admin', 'Administrador', '2016-06-09', 'X', 123456789, 'admin@admin.admin', 'calle falsa 123', 15, 1, true, 15, 1, 1, '2016-06-09 00:24:49'),
+(6, '177008796', 'Joseph', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Perez', 'Carmona', '1991-03-26', 'M', 50010078, 'jose.perez@peluka.info', 'antonio varas 666', 20, 2, true, 2, 2, 2, '2016-06-30 16:59:35'),
+(7, '186948483', 'Jonathan', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Espinoza', 'Brain', '1994-01-01', 'M', 23004500, 'jonathanespinozab@gmail.com', 'antonio varas 666', 35, 3, false, 0, 2, 3, '2016-06-30 16:59:35');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
