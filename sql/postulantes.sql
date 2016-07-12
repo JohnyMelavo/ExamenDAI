da3e6b552e6fc3361a449efe0cf200d5cefe0505
jomipeca <jose.perez@peluka.info>
2016-07-12 11:25:39 -0400

    SQL 1.0

---
+++ sql/postulantes.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-07-2016 a las 03:20:23
+-- Versión del servidor: 5.6.16
+-- Versión de PHP: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `postulantes`
+--
+CREATE DATABASE IF NOT EXISTS `postulantes` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `postulantes`;
+
+-- --------------------------------------------------------
+
+
+--
+-- Estructura de tabla para la tabla `comunas`
+--
+
+CREATE TABLE IF NOT EXISTS `comunas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(70) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=86 ;
+
+--
+-- Volcado de datos para la tabla `comunas`
+--
+
+INSERT INTO `comunas` (`id`, `descripcion`) VALUES
+(1, 'Alhué'),
+(2, 'Buin'),
+(3, 'Calera de Tango'),
+(4, 'Cerrillos'),
+(5, 'Cerro Navia'),
+(6, 'Colina'),
+(7, 'Conchalí'),
+(8, 'Curacaví'),
+(9, 'El Bosque '),
+(10, 'El Monte'),
+(11, 'Estación Central '),
+(12, 'Huechuraba'),
+(13, 'Independencia'),
+(14, 'Isla de Maipo'),
+(15, 'Lampa '),
+(16, 'La Cisterna'),
+(17, 'La Florida'),
+(18, 'La Granja'),
+(19, 'La Pintana'),
+(20, 'La Reina'),
+(21, 'Las Condes'),
+(22, 'Lo Barnechea'),
+(23, 'Lo Espejo'),
+(24, 'Lo Prado'),
+(25, 'Macul'),
+(26, 'Maipú'),
+(27, 'María Pinto'),
+(28, 'Melipilla'),
+(29, 'Ñuñoa'),
+(30, 'Tiltil'),
+(31, 'Padre Hurtado'),
+(32, 'Pedro Aguirre Cerda'),
+(33, 'Peñaflor'),
+(34, 'Peñalolén'),
+(35, 'Pirque '),
+(36, 'Providencia'),
+(37, 'Pudahuel'),
+(38, 'Puente Alto'),
+(39, 'Talagante'),
+(40, 'San Ramon'), 
+(41, 'San Bernardo'),
+(42, 'San Miguel'),
+(43, 'San Joaquín'),
+(44, 'San José de Maipo'),
+(45, 'Santiago'),
+(46, 'San Pedro'),
+(47, 'Paine'),
+(48, 'Quilicura'),
+(49, 'Quinta Normal'),
+(50, 'Recoleta'),
+(51, 'Renca'),
+(52, 'Recoleta'),
+(53, 'Renca'),
+(54, 'Vitacura');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `educacion`
+--
+
+CREATE TABLE IF NOT EXISTS `educacion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Volcado de datos para la tabla `educacion`
+--
+
+INSERT INTO `educacion` (`id`, `descripcion`) VALUES
+(1, 'Profesional'),
+(2, 'Tecnico'),
+(3, 'Media'),
+(4, 'Basica'),
+(5, 'No Posee');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modalidad`
+--
+
+CREATE TABLE IF NOT EXISTS `modalidad` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Volcado de datos para la tabla `modalidad`
+--
+
+INSERT INTO `modalidad` (`id`, `descripcion`) VALUES
+(1, 'Diurna'),
+(2, 'Vespertina');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+CREATE TABLE IF NOT EXISTS `curso` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Volcado de datos para la tabla `curso`
+--
+
+INSERT INTO `curso` (`id`, `descripcion`) VALUES
+(1, 'Java'),
+(2, '.NET'),
+(3, 'PHP');
+
+-- --------------------------------------------------------
+
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rut` varchar(10) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `apellido_pat` varchar(255) NOT NULL,
+  `apellido_mat` varchar(15) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `sexo` varchar(1) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `direccion` varchar(60) NOT NULL,
+  `id_comuna` varchar(25) NOT NULL,
+  `educacion` varchar(20) NOT NULL,
+  `experiencia` boolean NOT NULL,
+  `experiencia_anios` int(3),
+  `modalidad` int(11) NOT NULL,
+  `curso` int(11) NOT NULL,
+  `user_created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`rut`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`user_id`, `rut`, `nombre`, `password`, `apellido_pat`, `apellido_mat`, `fecha_nacimiento`, `sexo`, `telefono`, `email`, `direccion`, `id_comuna`, `educacion`, `experiencia`, `experiencia_anios`, `modalidad`, `curso`, `user_created_at`) VALUES
+(5, '123456789', 'Admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'admin', 'Administrador', '2016-06-09', 'X', '123456789', 'admin@admin.admin', 'calle falsa 123', 15, 1, true, 15, 0, 1, 1, '2016-06-09 00:24:49'),
+(6, '177008796', 'Joseph', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Perez', 'Carmona', '1991-03-26', 'M', '50010078', 'jose.perez@peluka.info', 'antonio varas 666', 20, 2, true, 2, 1, 2, 2, '2016-06-30 16:59:35');
+(8, '186948483', 'Jonathan', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Espinoza', 'Brain', '1994-00-00', 'M', '23004500', 'jonathanespinozab@gmail.com', 'antonio varas 666', 35, 3, false, 0, 1, 3, '2016-06-30 16:59:35');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
