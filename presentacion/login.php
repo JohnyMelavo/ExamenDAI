8d5878299b7b5c6e9358421320aa2b3db53b3194
Joseph Perez <jose.perez@peluka.info>
2016-07-13 00:39:47 -0300

    correccion diseño

--- presentacion/login.php
+++ presentacion/login.php
@@ -19,8 +19,8 @@ if (isset($_SESSION["oUsuario"])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body class="blurBg-true" style="background-color:#EBEBEB">
-        <link rel="stylesheet" href="./css/formoid-solid-green.css" type="text/css">
-        <script type="text/javascript" src="./js/jquery.min.js"></script>
+        <link rel="stylesheet" href="../css/formoid-solid-green.css" type="text/css">
+        <script type="text/javascript" src="../js/jquery.min.js"></script>
         <?php
         require ('../form/formLogin.php');
         ?>
