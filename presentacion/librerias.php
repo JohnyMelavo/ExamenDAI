6f6360b35090de321f30571a21eacce4cf4cdf2b
Joseph Perez <jose.perez@peluka.info>
2016-07-13 00:54:12 -0300

    correccion minima

--- presentacion/librerias.php
+++ presentacion/librerias.php
@@ -1,7 +1,7 @@
 <?php
 include ('constantes.php');
-include (PATHAPP.'../lib/db_funciones.php');
-include (PATHAPP.'../clases/Usuario.php');
+include (PATHAPP.'./lib/db_funciones.php');
+include (PATHAPP.'./clases/Usuario.php');
 ?>
 
-<!-- Revisar si db_funciones funciona, de otra forma quitar un punto(caracter) -->
+<!-- NO CAMBIAR CANTIDAD DE PUNTOS EN ESTE ARCHIVO -->
