08b98cee649aff4a272c24dae06dd688c1155d0a
Joseph Perez <jose.perez@peluka.info>
2016-07-13 00:42:31 -0300

    correccion minima

--- presentacion/librerias.php
+++ presentacion/librerias.php
@@ -1,5 +1,7 @@
 <?php
 include ('constantes.php');
-include (PATHAPP.'./lib/db_funciones.php');
-include (PATHAPP.'./clases/Usuario.php');
+include (PATHAPP.'../lib/db_funciones.php');
+include (PATHAPP.'../clases/Usuario.php');
 ?>
+
+<!-- Revisar si db_funciones funciona, de otra forma quitar un punto(caracter) -->
