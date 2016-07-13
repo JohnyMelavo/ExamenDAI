c8b867e73ae3fbbbf252b0cc0daf639c1210650b
Joseph <jose.perez@peluka.info>
2016-07-13 20:44:41 -0300

    Last changes

--- presentacion/detallePostulante.php
+++ presentacion/detallePostulante.php
@@ -2,6 +2,13 @@
 include 'librerias.php';
 $post=new Usuario();
 $rut = $_GET['srut'];
-$post->DeleteCliente($rut);
+
+$post->LeerDatos($rut);
 
 ?>
+
+
+<script>
+    alert("Pagina en mantencion, disculpe las molestias");
+    document.location.href = '../index.php' ;
+</script>
