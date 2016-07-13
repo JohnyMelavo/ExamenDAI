40b581db3d0eac821a7e32edd0f5a251986ff6a4
Joseph <jose.perez@peluka.info>
2016-07-13 17:59:06 -0300

    Insert 99%

--- accform/accPostulacion.php
+++ accform/accPostulacion.php
@@ -1,9 +1,39 @@
 <?php
 // JONA LEE ESTO: CUANDO INGRESES AQUI, COLOCA QUE LA CLAVE DEL USUARIO ES 0 SOLAMENTE ESO.
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+include ('../presentacion/librerias.php');
+$usr = new Usuario();
+$rut = $_POST['run'] . $_POST['run2'];
+$direccion = $_POST['direccion'];
+$snombre = $_POST['nombre'];
+$comuna = $_POST['comuna'];
+$sapaterno = $_POST['appaterno'];
+$educacion = $_POST['educacion'];
+$samaterno = $_POST['apmaterno'];
+$fechanac = $_POST['fechanacimiento'];
+$exper = $_POST['experiencia'];
+if($exper === 1){
+    $experanios = $_POST['expanios'];
+} else {
+    $experanios = 0;
+}
+$sexo = $_POST['sexo'];
+$fono = $_POST['telefono'];
+$modal = $_POST['modalidad'];
+$curso = $_POST['curso'];
+$email = $_POST['mail'];
+$spass = "0";
+if($usr->CreaCliente($rut, $snombre, $spass, $sapaterno, $samaterno, $fechanac, $sexo, $fono, $email, $direccion, $comuna, $educacion, $exper==1? true : false, $experanios, $modal, $curso)){
+    ?>
+<script>
+    alert('Se ha registrado con éxito!');
+    document.location.href = "<?= PATHURL ?>index.php";
+</script>
+<?php } else{
+    ?> 
+<script>
+    alert('Servidor ocupado, intente nuevamente mas tarde!');
+    document.location.href = "<?= PATHURL ?>index.php";
+</script>
+        <?php
+}
+?>
