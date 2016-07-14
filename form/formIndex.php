3af8f237d1353aad70465ce521e15361f5f8243e
Jonathan Espinoza <jonathanespinozab@gmail.com>
2016-07-13 21:29:34 -0300

    Agregar boton listado.

--- form/formIndex.php
+++ form/formIndex.php
@@ -5,7 +5,7 @@
     }
     ?>
 </div>
-<form class="formoid-solid-green" style="background-color:#aaaaaa;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:560px;min-width:150px">
+<form class="formoid-solid-green" style="background-color:#aaaaaa;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:860px;min-width:350px">
     <div class="title">
         <h2>Bienvenido al portal de postulaciones.</h2>
     </div>
@@ -13,8 +13,13 @@
     <br>    
     <div class="submit">
         <center>
-            <input type="button" value="Portal de Postulacion" onclick="location.href = 'presentacion/postulacion.php'">
-            <input type="button" value="Cambiar Contraseña" onclick="location.href = 'presentacion/changepwd.php'"><br>
+            <input type="button" value="Ingresar Postulacion" onclick="location.href = 'presentacion/postulacion.php'">
+            <input type="button" value="Estado Postulaciones" onclick="location.href = 'presentacion/listadoPostulaciones.php'">
+            <input type="button" value="Cambiar Contraseña" onclick="location.href = 'presentacion/changepwd.php'">
+            <br>
+            <br>
+            <br>
+            <br>
             <input type="button" value="Cerrar Sesión" onclick="location.href = 'presentacion/logout.php'">
         </center>
 
